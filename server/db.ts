18d622ee30416cf69ae819be4e01daaf2ea6f36b
Asantha20535 <s20535@sci.pdn.ac.lk>
2025-07-26 19:42:50 +0530

    g

--- server/db.ts
+++ server/db.ts
@@ -1,15 +1,26 @@
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
-import ws from "ws";
+// import { Pool, neonConfig } from '@neondatabase/serverless';
+// import { drizzle } from 'drizzle-orm/neon-serverless';
+// import ws from "ws";
 import * as schema from "@shared/schema";
+//
+// neonConfig.webSocketConstructor = ws;
+//
+// if (!process.env.DATABASE_URL) {
+//   throw new Error(
+//     "DATABASE_URL must be set. Did you forget to provision a database?",
+//   );
+// }
+//
+// export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+// export const db = drizzle({ client: pool, schema });
 
-neonConfig.webSocketConstructor = ws;
 
-if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
-  );
-}
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+// import { schema } from './drizzle/schema';
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-export const db = drizzle({ client: pool, schema });
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL, // should be like postgres://user:pass@localhost:5432/db
+});
+export const db = drizzle(pool, { schema });
+
